453e6d26681c216980700f2f67e215a99956456f
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-02-24 09:32:42 +0700

    soal 4

--- MatakuliahDemo06.java
+++ MatakuliahDemo06.java
@@ -2,33 +2,24 @@ import java.util.Scanner;
 public class MatakuliahDemo06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah06[] arrayOfMatakuliah = new Matakuliah06[3];
-        String kode, nama,dummy;
-        int sks, jumlahJam;
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1) + ":");
-            System.out.print("Kode : ");
-             kode = sc.nextLine();
-            System.out.print("Nama : ");
-            nama = sc.nextLine();
-            System.out.print("SKS : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("----------------------------");
-            arrayOfMatakuliah[i] = new Matakuliah06(kode, nama, sks, jumlahJam);
-        }
+        // Nomor 4
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int jml = Integer.parseInt(sc.nextLine());
+        Matakuliah06[] arrayOfMatakuliah = new Matakuliah06[jml];
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Data Matakuliah ke-" + (i + 1) + ":");
-            System.out.println("Kode : " + arrayOfMatakuliah[i].kode);
-            System.out.println("Nama : " + arrayOfMatakuliah[i].nama);
-            System.out.println("SKS : " + arrayOfMatakuliah[i].sks);
-            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam);
-            System.out.println("----------------------------");
+
+        // Proses Input menggunakan method tambahData
+        for (int i = 0; i < jml; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i] = new Matakuliah06(); // Instansiasi objek
+            arrayOfMatakuliah[i].tambahData(sc);    // Panggil method tambahData
+            System.out.println("---------------------------");
         }
-    }
-    
+        // Proses Output menggunakan method cetakInfo
+        System.out.println("\n=== Hasil Input Data Matakuliah ===");
+        for (int i = 0; i < jml; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i].cetakInfo(); // Panggil method cetakInfo
+        }
+    }   
 }
